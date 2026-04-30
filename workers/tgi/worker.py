9bc9ba11c50ce2cc3b4400e06eb3a45f5f32f34e
Lucas Armand <lucas@vast.ai>
2026-04-30 14:04:39 -0700

    Increase TGI benchmark tokens to 500

--- workers/tgi/worker.py
+++ workers/tgi/worker.py
@@ -35,7 +35,7 @@ def benchmark_generator() -> dict:
     benchmark_data = {
         "inputs": prompt,
         "parameters": {
-            "max_new_tokens": 128,
+            "max_new_tokens": 500,
             "temperature": 0.7,
             "return_full_text": False
         }
